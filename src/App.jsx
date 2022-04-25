ed25d8dccda35fe682582287aa62a0824ac0410f
aimeejenner <aimeejenner@gmail.com>
2022-04-25 16:59:02 +0100

    Counter component created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+import React, { useState } from 'react';
+import Counter from './components/Counter/Counter';
+
+const App = () => {
+  return (
+    <Counter />
+  );
+}
+
+export default App;
