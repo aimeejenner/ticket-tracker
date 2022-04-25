686dc1f51f5b7575e3df068f134578798ca5befc
aimeejenner <aimeejenner@gmail.com>
2022-04-25 17:41:21 +0100

    Added employees data

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import styles from './App.module.scss';
 import React, { useState } from 'react';
 import Counter from './components/Counter/Counter';
 import Employee from './components/Employee/Employee';
+import employees from "./data/employees";
 
 const App = () => {
+  const getEmployeeJsx = (employee) => (
+    <Employee />
+  )
+
   return (
-      <Employee />
+      <Employee employee={employees[1]} />
   );
 }
 
