884b852384d4f32b986ece4c3c11bece49e58b7b
aimeejenner <aimeejenner@gmail.com>
2022-04-25 17:08:01 +0100

    Created Employee component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import styles from './App.module.scss';
 import React, { useState } from 'react';
 import Counter from './components/Counter/Counter';
+import Employee from './components/Employee/Employee';
 
 const App = () => {
   return (
+    <Employee />,
     <Counter />
   );
 }
---
+++ src/components/Employee/Employee.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./Employee.module.scss";
+
+
+const Employee = () => {
+  return (
+    <div className="employee-container">
+        <h1>Employee name</h1>
+    </div>
+  )
+}
+
+export default Employee;
